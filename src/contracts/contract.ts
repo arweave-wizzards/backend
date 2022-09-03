24bc8b220544e397c6e018db3003eca57b5f0a83
dknuszkiewicz@gmail.com <dknuszkiewicz@gmail.com>
2022-09-04 00:12:11 +0200

    Revert "fix: add missing import statement in the contract"
    This reverts commit 2af487936c0a133603cd6ccde83fad3c41734e10.

--- src/contracts/contract.ts
+++ src/contracts/contract.ts
@@ -1,6 +1,5 @@
 import { BlogState } from './model/blog-state';
 import { BlogAction } from './model/blog-action';
-import { ContractResult } from './model/contract-result';
 
 export async function handle(
   state: BlogState,

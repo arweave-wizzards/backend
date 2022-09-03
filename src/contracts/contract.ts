2af487936c0a133603cd6ccde83fad3c41734e10
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-09-03 22:50:53 +0200

    fix: add missing import statement in the contract

--- src/contracts/contract.ts
+++ src/contracts/contract.ts
@@ -1,5 +1,6 @@
 import { BlogState } from './model/blog-state';
 import { BlogAction } from './model/blog-action';
+import { ContractResult } from './model/contract-result';
 
 export async function handle(
   state: BlogState,

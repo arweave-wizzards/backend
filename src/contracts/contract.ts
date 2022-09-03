613b982aae5ab1ad902dcea967a96b7dad5f5762
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-09-03 23:28:51 +0200

    create additional types

--- src/contracts/contract.ts
+++ src/contracts/contract.ts
@@ -5,4 +5,10 @@ import { ContractResult } from './model/contract-result';
 export async function handle(
   state: BlogState,
   action: BlogAction
-): Promise<ContractResult> {}
+): Promise<ContractResult> {
+  const input = action.input;
+  switch (input.function) {
+    case 'addPost':
+      return await addPost(state, action);
+  }
+}

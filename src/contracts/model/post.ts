b023fbfc70d6bb8e734c8b022db0cbb82dd37782
dknuszkiewicz@gmail.com <dknuszkiewicz@gmail.com>
2022-09-03 18:57:12 +0200

    feat: add models

---
+++ src/contracts/model/post.ts
@@ -0,0 +1,11 @@
+export interface Post {
+    id: number;
+    author: string;
+    content: string;
+    votes: {
+      addresses: string[];
+      status: number;
+    };
+    timestamp: number;
+    category: string
+  }
---
+++ src/contracts/model/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+    address: string;
+    role: string;
+    votes: number;
+    lastVotesClaimTimestamp: number;
+    streetCredit: number; //points of experience
+
+}
